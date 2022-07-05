1a712b4d6e5e0ea4f2a5e02d799c7d6b2f7b0742
edwinans <edyansari5@gmail.com>
2022-07-05 17:53:18 +0200

    update the target for guess

--- bin/main.v
+++ bin/main.v
@@ -5,7 +5,7 @@ From FreeSpec.Example.Guess Require Import Guess.
 From CoqFFI Require Import Int.
 
 Definition main : io_unit :=
-  let target : i63 := 2 in 
+  let target : i63 := 5 in 
   let max_attempt : nat := 10%nat in
   IO.unsafe_run (guess target max_attempt).
 
